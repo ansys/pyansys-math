9736ace41d39520ff3dcf6767de6d5ecec50f608
Camille <camille.latapie@ansys.com>
2022-12-08 14:02:12 +0100

    Changes related to pyansys/actions

---
+++ src/ansys/math/core/__init__.py
@@ -0,0 +1,23 @@
+"""This init file allows python to treat directories containing it as modules.
+
+Import any methods you want exposed at your library level here.
+
+For example, if you want to avoid this behavior:
+
+.. code::
+
+   >>> from ansys.product.library.module import add
+
+Then add the import within this module to enable:
+
+.. code::
+
+   >>> from ansys.product import library
+   >>> library.add(1, 2)
+
+.. note::
+   It's best to import the version here as well so it can be
+   referenced at the library level.
+
+"""
+from ansys.math.core import __version__
