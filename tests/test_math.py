43bf7d994e160708d0149bd438fc09d75e2d64a1
Camille <78221213+clatapie@users.noreply.github.com>
2023-06-08 12:18:19 +0200

    Code style checks

--- tests/test_math.py
+++ tests/test_math.py
@@ -5,7 +5,6 @@ import re
 from ansys.mapdl.core.errors import ANSYSDataTypeError, VersionError
 from ansys.mapdl.core.launcher import get_start_instance
 from ansys.mapdl.core.misc import random_string
-from ansys.tools.versioning.utils import server_meets_version
 import numpy as np
 import pytest
 from scipy import sparse
