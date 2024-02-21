151e2b6c256d078f09ed3591c44e222e0b63be76
Camille <78221213+clatapie@users.noreply.github.com>
2024-02-21 14:20:57 +0100

    maint: removing `pyvista.FIGURE_PATH`

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,11 +19,6 @@ pyvista.set_error_output_file("errors.txt")
 # must be less than or equal to the XVFB window size
 pyvista.global_theme.window_size = np.array([1024, 768])
 
-# Save figures in specified directory
-pyvista.FIGURE_PATH = os.path.join(os.path.abspath("./images/"), "auto-generated/")
-if not os.path.exists(pyvista.FIGURE_PATH):
-    os.makedirs(pyvista.FIGURE_PATH)
-
 # necessary when building the sphinx gallery
 pyvista.BUILDING_GALLERY = True
 pymath.BUILDING_GALLERY = True
