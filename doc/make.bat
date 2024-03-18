19e88aa4368c6508fa18f3037fdbb60e2b65f1eb
Camille <78221213+clatapie@users.noreply.github.com>
2024-03-18 18:14:48 +0100

    feat: adding new Makefile options (#286)
    Co-authored-by: Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>

--- doc/make.bat
+++ doc/make.bat
@@ -10,9 +10,12 @@ if "%SPHINXBUILD%" == "" (
 set SOURCEDIR=source
 set BUILDDIR=_build
 set EXAMPDIR=source\examples
+set SPHINXOPTS=-j auto --keep-going
 
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
+if "%1" == "pdf" goto pdf
+if "%1" == "html" goto html
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -30,6 +33,11 @@ if errorlevel 9009 (
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
+:html
+%SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+%SPHINXBUILD% -M linkcheck %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
 :clean
 rmdir /s /q %BUILDDIR% > /NUL 2>&1 
 rmdir /s /q %EXAMPDIR% > /NUL 2>&1 
@@ -38,6 +46,17 @@ goto end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+
+:pdf
+%SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+cd "%BUILDDIR%\latex"
+for %%f in (*.tex) do (
+pdflatex "%%f" --interaction=nonstopmode)
+if NOT EXIST ansys-math-core.pdf (
+	Echo "no pdf generated!"
+	exit /b 1)
+Echo "pdf generated!"
+goto end
 
 :end
 popd
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -207,3 +207,7 @@ htmlhelp_basename = "pyansysmathdoc"
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [(master_doc, "ansys.math.core", "ansys.math.core Documentation", [author], 1)]
+
+linkcheck_ignore = [
+    "https://www.ansys.com/*",
+]
