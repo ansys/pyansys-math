8aac1e6ffd1ad01b619b9b5a3f63b8acb832840f
RobPasMue <RobPasMue@users.noreply.github.com>
2024-03-26 07:56:00 +0000

    DOC: update development documentation f29b17fa729d820011a32df9e6530c1a12bc6f2d

---
+++ version/0.1/_downloads/fcb0908ed05c02b9fce3ac4c52b338f0/solve_sparse_matrix.py
@@ -0,0 +1,138 @@
+"""
+Perform sparse factorization and solve operations
+-------------------------------------------------
+
+Using PyAnsys Math, you can solve linear systems of equations
+based on sparse or dense matrices.
+
+"""
+# Perform required imports and start PyAnsys
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Perform required imports.
+
+from ansys.mapdl.core.examples import vmfiles
+import matplotlib.pyplot as plt
+
+import ansys.math.core.math as pymath
+
+# Start PyAnsys Math as a server.
+mm = pymath.AnsMath()
+
+###############################################################################
+# Factorize and solve sparse linear systems
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Run a MAPDL solve to create a Full file. This code
+# uses a model from the official verification manual.
+#
+# After a solve command, the ``FULL`` file contains the assembled stiffness
+# matrix, mass matrix, and load vector.
+#
+out = mm._mapdl.input(vmfiles["vm152"])
+
+###############################################################################
+# List files in current directory
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# List the files in current directory.
+#
+mm._mapdl.list_files()
+
+###############################################################################
+# Extract stiffness matrix
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Extract the stiffness matrix from the FULL file in a sparse
+# matrix format. For help on the ``stiff`` function, use the
+# ``help(mm.stiff)`` command.
+#
+# Print dimensions of sparse matrix
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Print the dimensions of the sparse matrix.
+
+fullfile = mm._mapdl.jobname + ".full"
+k = mm.stiff(fname=fullfile, name="K")
+k
+
+################################################################################
+# Copy AnsMath sparse matrix to SciPy CSR matrix and plot
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Copy the AnsMath sparse matrix to a SciPy CSR matrix. Then, plot the
+# graph of the sparse matrix.
+
+pk = k.asarray()
+plt.spy(pk, color="orange", markersize=3)
+plt.title("AnsMath sparse matrix")
+plt.show()
+
+###############################################################################
+# Get a copy of sparse matrix as a NumPy array
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get a copy of the ``k`` sparse matrix as a NumPy array
+
+ky = k.asarray()
+ky
+
+###############################################################################
+# Extract load vector from FULL file and print norm
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Extract the load vector from the FULL file and print the norm of this
+# vector.
+b = mm.rhs(fname=fullfile, name="B")
+b.norm()
+
+###############################################################################
+# Get a copy of load vector as a NumPy array
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get a copy of the load vector as a NumPy array.
+
+by = b.asarray()
+
+###############################################################################
+# Factorize stiffness matrix
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Factorize the stiffness matrix using PyAnsys Math.
+#
+s = mm.factorize(k)
+
+###############################################################################
+# Solve linear system
+# ~~~~~~~~~~~~~~~~~~~
+# Solve the linear system.
+
+x = s.solve(b)
+
+###############################################################################
+# Print norm** of solution vector
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Print the norm of the solution vector.
+
+x.norm()
+
+###############################################################################
+# Check accuracy of solution
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Check the accuracy of the solution by verifying that
+# :math:`KX - B = 0`.
+
+kx = k.dot(x)
+kx -= b
+print("Residual error:", kx.norm() / b.norm())
+
+###############################################################################
+# Get a summary of allocated objects
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get a summary of all allocated AnsMath objects.
+
+mm.status()
+
+######################################################################
+# Delete all AnsMath objects
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Delete all AnsMath objects.
+
+mm.free()
+
+###############################################################################
+# Stop PyAnsys Math
+# ~~~~~~~~~~~~~~~~~
+# Stop PyAnsys Math.
+
+mm._mapdl.exit()
---
+++ version/stable/_downloads/569de3e8608c800f29ed64e48c222fef/solve_dense_matrix.py
@@ -0,0 +1,104 @@
+"""
+Use PyAnsys Math to solve a dense matrix linear system
+------------------------------------------------------
+This example shows how to use PyAnsys Math to solve a dense matrix linear system.
+
+"""
+# Perform required imports and start PyAnsys
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Perform required imports.
+
+import time
+
+import matplotlib.pyplot as plt
+import numpy.linalg as npl
+
+import ansys.math.core.math as pymath
+
+# Start PyAnsys Math as a server.
+mm = pymath.AnsMath()
+
+###############################################################################
+# Allocate dense matrix
+# ~~~~~~~~~~~~~~~~~~~~~
+# Allocate a dense matrix in the MAPDL workspace.
+
+mm._mapdl.clear()
+dim = 1000
+a = mm.rand(dim, dim)
+b = mm.rand(dim)
+x = mm.zeros(dim)
+
+###############################################################################
+# Copy matrices as NumPy arrays
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Copy the matrices as NumPy arrays before they are modified by
+# a factorization call.
+#
+a_py = a.asarray()
+b_py = b.asarray()
+
+###############################################################################
+# Solve using PyAnsys Math
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Solve the dense matrix linear system using PyAnsys Math.
+#
+print(f"Solving a ({dim} x {dim}) dense linear system using PyAnsys Math...")
+
+t1 = time.time()
+s = mm.factorize(a)
+x = s.solve(b, x)
+t2 = time.time()
+pymath_time = t2 - t1
+print(f"Elapsed time to solve the linear system using PyAnsys Math: {pymath_time} seconds")
+
+###############################################################################
+# Get norm of solution
+# ~~~~~~~~~~~~~~~~~~~~
+# Get the norm of the PyAnsys Math solution.
+mm.norm(x)
+
+
+###############################################################################
+# Solve using NumPy
+# ~~~~~~~~~~~~~~~~~
+# Solve the dense matrix linear system using NumPy.
+#
+print(f"Solving a ({dim} x {dim}) dense linear system using NumPy...")
+
+t1 = time.time()
+x_py = npl.solve(a_py, b_py)
+t2 = time.time()
+numpy_time = t2 - t1
+print(f"Elapsed time to solve the linear system using NumPy: {numpy_time} seconds")
+
+###############################################################################
+# Plot elapsed times
+# ~~~~~~~~~~~~~~~~~~~
+# Plot the elapsed times for PyAnsys Math and Numpy to solve the dense
+# matrix linear system.
+#
+max_time = max(pymath_time, numpy_time)
+fig = plt.figure(figsize=(12, 10))
+ax = plt.axes()
+x = ["PyAnsys Math", "NumPy"]
+y = [pymath_time, numpy_time]
+plt.title("Elapsed time to solve the linear system")
+plt.ylim([0, max_time + 0.2 * max_time])
+plt.ylabel("Elapsed time (s)")
+ax.bar(x, y, color="orange")
+plt.show()
+
+###############################################################################
+# Get norm of solution
+# ~~~~~~~~~~~~~~~~~~~~
+# Get the norm of the NumPy solution.
+
+npl.norm(x_py)
+
+###############################################################################
+# Stop PyAnsys Math
+# ~~~~~~~~~~~~~~~~~
+# Stop PyAnsys Math.
+
+mm._mapdl.exit()
